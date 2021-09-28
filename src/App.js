8ef91d3093b820f01db3aaff5f55ac08e586220c
Laura <laura.jarventie@edu.bc.fi>
2021-09-28 14:38:03 +0300

    first skelaton

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Tähän tulee Artisaanzit reduxilla tehtynä!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
