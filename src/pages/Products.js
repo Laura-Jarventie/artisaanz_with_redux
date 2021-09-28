740c0c1975fd3c0dea80ab4c9543678431a7f0bc
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-09-28 16:00:58 +0300

    add landing page and App.css  and list installation instructions in readme

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Products = () => {
     return (
         <div>
-            <h1>Products</h1>
+            <h1>Tuotteet on tulossa tänne tuota pikaa...</h1>
         </div>
     );
 };
