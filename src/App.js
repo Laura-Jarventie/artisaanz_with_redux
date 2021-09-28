e8345a16ec8e6f615a7563ca6487e679bd6f5f06
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-09-28 16:16:30 +0300

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { initializeProducts } from "./store/actions/actions";
 import Main from "./Containers/Main";
+import Header from "./Containers/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,7 +17,9 @@ function App() {
 
   return (
     <Router>
+      <Header />
       <Main />
+      <Footer />
     </Router>
   );
 }
