3f0f04056337ec0d281270b261c72913379c7e11
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-10-11 14:34:27 +0300

    create Makers.js file

---
+++ src/pages/Makers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+
+const Makers = () => {
+  const dispatch = useDispatch();
+
+  return <div><h1>Tänne tulee artesaanit</h1></div>;
+};
+
+export default Makers;
