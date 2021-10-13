4934704f60ab7d70e7eb04e00c4cab0c18db179a
Vuorikko Salla <salla.vuorikko@gmail.com>
2021-10-13 09:59:48 +0300

    In services, rename getAll to getAllProducts and make a new function getMakers

--- src/services/services.js
+++ src/services/services.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const baseUrl = 'https://artisaanz.herokuapp.com/product';
+const baseUrl = 'https://artisaanz.herokuapp.com';
 
-export const getAll = async () => {
-    const response = await axios.get(baseUrl + '/all');
+export const getAllProducts = async () => {
+    const response = await axios.get(baseUrl + '/product/all');
+    return response.data;
+};
+
+export const getMakers = async () => {
+    const response = await axios.get(baseUrl + '/seller/all');
     return response.data;
 };
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,10 +1,10 @@
-import { getAll } from "../../services/services";
+import { getAllProducts } from "../../services/services";
 
 export const INIT_PRODUCTS = "INIT_PRODUCTS";
 
 export const initializeProducts = () => {
     return async (dispatch) => {
-      const products = await getAll();
+      const products = await getAllProducts();
       dispatch({
         type: INIT_PRODUCTS,
         data: products,
