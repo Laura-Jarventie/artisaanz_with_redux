f61c3cec7497bf2904a5ac7b0c82fd6f9d20b236
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-10-20 13:24:25 +0300

    modified product card, changed font, crated CartCard.css, modified font size on front page

--- src/Containers/CartCard.js
+++ src/Containers/CartCard.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ListGroup from "react-bootstrap/ListGroup";
+import "./CartCard.css";
 
 const CartCard = ({ nimi, kuva, hinta, removeBtn }) => {
   return (
     <div className="cartCard">
-      <img src={kuva[0].kuva} alt="tuotteen kuva" height="250" width="250" />
-      <p>{nimi}</p>
-      <p>{hinta} €</p>
-      <button onClick={() => console.log("poista korista")}>{removeBtn}</button>
+      <div className="product-image">
+        <img
+          src={kuva[0].kuva}
+          alt="tuotteen kuva"
+          height="350px"
+          width="250px"
+        />
+      </div>
+      <div className="product-info">
+        <div className="product-text">
+          <h1>{nimi}</h1>
+          <p>{hinta} €</p>
+          <button className="removeBtn" onClick={() => console.log("poista korista")}>
+            {removeBtn}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Containers/ProductCard.js
+++ src/Containers/ProductCard.js
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom";
 const ProductCard = ({ id, nimi, kuva, artesaani, hinta, kategoria }) => {
   return (
     <div className="productCard">
-      <Link to={`/tuotteet/${id}`} >
-        <p>{nimi}</p>
-      </Link>
-      <Link to={`/tuotteet/${id}`}>
-        <img src={kuva[0].kuva} alt="tuotteen kuva" />
-      </Link>
-      <p>Artesaani: {artesaani} </p>
-      <p>Hinta: {hinta} €</p>
-      <p>Kategoria: {kategoria}</p>
+        <Link to={`/tuotteet/${id}`}>
+          <img src={kuva[0].kuva} alt="tuotteen kuva" />
+        </Link>
+      <div className="product-details">
+        <span className="product-category"> {kategoria}</span>
+        <Link to={`/tuotteet/${id}`}>
+          <h4>{nimi}</h4>
+        </Link>
+        <p>{artesaani} </p>
+        <div className="product-bottom-details">
+          <div className="product-price">{hinta} €</div>
+        </div>
+      </div>
     </div>
   );
 };
