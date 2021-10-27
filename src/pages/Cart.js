b7d47af9f2a0b5d874cca822f4d637a5e677338c
Akseli <s2100140@edu.bc.fi>
2021-10-27 15:43:20 +0300

    sending data to stripe through separate server with express roughly works

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,6 +4,35 @@ import CartCard from "../Containers/CartCard";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
+  const maksa = () => {
+    fetch("http://localhost:3001/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 1 },
+          { id: 2, quantity: 1 },
+          { id: 3, quantity: 1 },
+          { id: 4, quantity: 1 },
+          { id: 5, quantity: 1 },
+          { id: 6, quantity: 1 },
+          { id: 7, quantity: 1 },
+          { id: 8, quantity: 1 },
+        ],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then(({ url }) => {
+        window.location = url;
+      })
+      .catch((e) => [console.log(e.error)]);
+  };
+
   const cartList = useSelector((state) => state.cart);
 
   const cartItems = cartList.map((tuote) => {
@@ -31,6 +60,7 @@ const Cart = () => {
         {" "}
         Herokuun maksamaan tästä!{" "}
       </a>
+      <button onClick={() => maksa()}>Maksa nodella</button>
     </main>
   );
 };
